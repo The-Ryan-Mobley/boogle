c4cd2c51adba2b92c37469a7b0b5ba897afebc2e
Ryan Mobley <ryan.mobley94@gmail.com>
2019-11-05 11:15:24 -0600

    can create users

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 const router = require("express").Router();
 const authorizer = require("../../controller/authorizer");
-const librarian = require("../../controller/Librarian");
 
-router.route("/api/user") //need to conver function vars
-    .get(authorizer.addUser)
-    .post(authorizer.loginUser);
+
+router.route("/new").post(authorizer.addUser);
+router.route("/login").get(authorizer.loginUser);
+   
 
 //https://www.googleapis.com/books/v1/volumes?q=
 module.exports = router;
