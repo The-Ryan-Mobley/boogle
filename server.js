a5dbf6f4b52ea04930f595c296e8f55c4b9bbcc0
Ryan Mobley <ryan.mobley94@gmail.com>
2019-11-07 17:51:04 -0600

    please work

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI || 
