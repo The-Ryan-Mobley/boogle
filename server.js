4c8cf2c250e62fb9b386afb8f99499e7b77becb0
Ryan Mobley <ryan.mobley94@gmail.com>
2019-11-06 21:04:57 -0600

    deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(routes);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "./client", "build", "index.html")))
 }
 
 mongoose.connect(process.env.MONGODB_URI || 
