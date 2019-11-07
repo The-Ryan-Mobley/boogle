6d1c08c9b2ea6e47a3e8c010b399777077526741
Ryan Mobley <ryan.mobley94@gmail.com>
2019-11-07 00:13:39 -0600

    ?

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
 }
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 mongoose.connect(process.env.MONGODB_URI || 
   "mongodb://localhost/boogle");
