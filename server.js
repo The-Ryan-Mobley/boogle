67ca633a6992c9c76ddc8cc66479c421fcf898fd
Ryan Mobley <ryan.mobley94@gmail.com>
2019-11-07 21:25:34 -0600

    u

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 1337;
 const app = express();
 
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.use(routes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
-}
+
+
 // app.get('*',(req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 // });
