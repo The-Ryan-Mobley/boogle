d445e1ec485a0736c54de31746d24242d1efc636
Ryan Mobley <ryan.mobley94@gmail.com>
2019-11-07 18:01:14 -0600

    work?

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
 
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || 
   "mongodb://localhost/boogle");
