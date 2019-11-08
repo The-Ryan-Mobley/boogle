d527bc801572a7a1d1f1f1ac20a0c705ae6e9a62
Ryan Mobley <ryan.mobley94@gmail.com>
2019-11-07 21:22:05 -0600

    ?

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
   app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*',(req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 mongoose.connect(process.env.MONGODB_URI || 
   "mongodb://localhost");
